d4512a5ce8dc5afbe9d25116a047b9db1949374e
sercan94 <sercan.imir1994@gmail.com>
2025-05-11 11:57:31 +0300

    SwaggerConfig eklendi.

---
+++ src/main/java/com/project/humanresource/config/SwaggerConfig.java
@@ -0,0 +1,24 @@
+package com.project.humanresource.config;
+
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@OpenAPIDefinition(
+        info = @Info(title = "Muhammet HOCA", version = "1.0"),
+        security = @SecurityRequirement(name = "bearerAuth") // Swagger UI için yetkilendirme gerektir
+)
+@SecurityScheme(
+        name = "bearerAuth",
+        scheme = "bearer",
+        type = SecuritySchemeType.HTTP,
+        bearerFormat = "JWT"
+)
+
+public class SwaggerConfig {
+}
