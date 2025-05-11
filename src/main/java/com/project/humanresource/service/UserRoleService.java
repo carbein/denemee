c99f89a4f3ed30a40ffccda1016a904508fe8d01
Goksel <cegoksel@gmail.com>
2025-05-11 10:27:25 +0300

    UserRoleService: findRoleByUserId metodu eklendi, repository kullanımı düzeltildi

--- src/main/java/com/project/humanresource/service/UserRoleService.java
+++ src/main/java/com/project/humanresource/service/UserRoleService.java
@@ -1,6 +1,8 @@
 package com.project.humanresource.service;
 
+import com.project.humanresource.entity.User;
 import com.project.humanresource.entity.UserRole;
+import com.project.humanresource.repository.UserRepository;
 import com.project.humanresource.repository.UserRoleRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -10,10 +12,12 @@ import java.util.Optional;
 @Service
 public class UserRoleService {
     private final UserRoleRepository userRoleRepository;
+    private final UserRepository userRepository;
 
     @Autowired
-    public UserRoleService(UserRoleRepository userRoleRepository) {
+    public UserRoleService(UserRoleRepository userRoleRepository, UserRepository userRepository) {
         this.userRoleRepository = userRoleRepository;
+        this.userRepository = userRepository;
     }
 
     public UserRole save(UserRole userRole) {
@@ -22,5 +26,11 @@ public class UserRoleService {
 
     public Optional<UserRole> findByName(String name) {
         return userRoleRepository.findByName(name);
+    }
+
+    public UserRole findRoleByUserId(Long userId) {
+        return userRepository.findById(userId)
+            .map(User::getUserRole)
+            .orElse(null);
     }
 } 
